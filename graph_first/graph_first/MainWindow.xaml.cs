69c9ba2f582ec667741bb24de448fef7f2182e34
Rafal <jagielski.rafal.uwm@tlen.pl>
2020-03-20 21:50:47 +0100

    Completed graph generating

---
+++ graph_first/graph_first/MainWindow.xaml.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace graph_first
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        public class Tree
+        {
+            public List<Node> nodes;
+            public List<Edge> edges;
+            
+            public Tree()
+            {
+                nodes = new List<Node>();
+                edges = new List<Edge>();
+            }
+            public void addNode(Node node)
+            {
+                nodes.Add(node);
+            }
+
+            public void addEdge(Edge edge)
+            {
+                edges.Add(edge);
+            }
+
+            public void createBranches(Node root)
+            {
+                if ((root!=null)&&(root.value >= 7)) return;
+                Node newRoot = root;
+                Node tmp;
+                int value;
+                string player;
+                int result = 2;
+                if (newRoot == null)
+                {
+                    newRoot = new Node(0, 0);
+                    this.addNode(newRoot);
+                    //Console.WriteLine("new root created \n");
+                }
+                if (newRoot.player == "prot") player = "ant";
+                else player = "prot";
+                for (int i = 0; i < 3; i++)
+                {
+                    value = newRoot.value + 4 + i;
+                    if ((value > 7) && (player == "prot")) result = 1;
+                    else if ((value > 7) && (player == "ant")) result = 3;
+                    tmp = new Node(newRoot.number+1+i, value, player, result, newRoot);
+                    this.addNode(tmp);
+                    this.addEdge(new Edge(newRoot, tmp, 4 + i));
+                    this.createBranches(tmp);
+                    //tmp.print();
+                }
+            }
+            public void print()
+            {
+                for (int i = 0; i < this.edges.Count(); i++)
+                {
+                    edges[i].print();
+                    Console.WriteLine("\n");
+                }
+            }
+            public void treeAsString()
+            {
+                int i=0;
+                string result = "";
+                string firstValues = this.edges[i].begin.value == 0 ? this.edges[i].value.ToString() : this.edges[i].begin.value.ToString() + this.edges[i].value;
+                for (i = 0; i < this.edges.Count(); i++)
+                {
+                    result += "\"" +this.edges[i].begin.player + ";\\n "+ this.edges[i].begin.value + ";\\n wynik= " + this.edges[i].begin.result +
+                        "\" -> \"" + this.edges[i].end.player + ";\\n " + this.edges[i].begin.value+"+"+this.edges[i].value + ";\\n wynik= " + this.edges[i].end.result+"\" " +
+                        "[label = \"" + this.edges[i].value + "\"];\n";
+                }
+                Console.WriteLine(result);
+            }
+        }
+        public class Node
+        {
+            public Node ancestor { get; set; }
+            public string player { get; set; }
+            public int number { get; set; }
+            public int value { get; set; }
+            public int result { get; set; } //1: przegrana 2: remis 3: wygrana
+
+            public Node(int _number, int _value, string _player = "prot", int _result = 2, Node _ancestor=null)
+            {
+                number=_number;
+                value=_value;
+                player = _player;
+                result = _result;
+                ancestor = _ancestor;
+            }
+            public void print()
+            {
+                Console.WriteLine(this.number + "=>" + this.value + "=>" + this.player + "=>" + this.result);
+            }
+            public string nodeAsString()
+            {
+                return this.number + "=>" + this.value + "=>" + this.player + "=>" + this.result;
+            }
+
+        }
+        public class Edge
+        {
+            public Node begin { get; set; }
+            public Node end { get; set; }
+            public int value { get; set; }
+
+            public Edge(Node _begin, Node _end, int _value)
+            {
+                begin=_begin;
+                end=_end;
+                value = _value;
+            }
+            public void print()
+            {
+                Console.WriteLine(begin.nodeAsString()+"---"+end.nodeAsString());
+            }
+
+        }
+
+        private void test_click(object sender, RoutedEventArgs e)
+        {
+        //    Node a = new Node(0, 0);
+        //    Node b = new Node(1, 4);
+        //    Node c = new Node(2, 5);
+        //    Node d = new Node(3, 6);
+
+
+        //    Edge ab = new Edge(a, b);
+        //    ab.print();
+        Tree tree = new Tree();
+        tree.createBranches(null);
+        tree.treeAsString();
+
+        }
+    }
+}
