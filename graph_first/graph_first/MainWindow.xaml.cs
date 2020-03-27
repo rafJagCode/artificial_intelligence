c09e30d7526d19c3913446a4969d056a4038cb06
Rafal <jagielski.rafal.uwm@tlen.pl>
2020-03-27 21:11:30 +0100

    Add popup backgroundWorker

--- graph_first/graph_first/MainWindow.xaml.cs
+++ graph_first/graph_first/MainWindow.xaml.cs
@@ -12,15 +12,20 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.ComponentModel;
 
 namespace graph_first
 {
     public partial class MainWindow : Window
     {
         static public int limit = 21;
+        public BackgroundWorker backgroundWorker1 = new BackgroundWorker();
         public MainWindow()
         {
             InitializeComponent();
+            backgroundWorker1.DoWork += backgroundWorker1_DoWork;
+            backgroundWorker1.ProgressChanged += backgroundWorker1_ProgressChanged;
+            backgroundWorker1.WorkerReportsProgress = true;
         }
 
         private void generateBtn(object sender, RoutedEventArgs e)
@@ -42,24 +47,34 @@ namespace graph_first
         {
 
             Clipboard.SetText(treeTextField.Text);
-            popupText.Text = "Copied to clipboard";
+            popupText.Text = "Tree copied to clipboard";
+            backgroundWorker1.RunWorkerAsync();
 
 
         }
         private void copySolutionBtn(object sender, RoutedEventArgs e)
         {
             Clipboard.SetText(solutionTextField.Text);
+            popupText.Text = "Solution copied to clipboard";
+            backgroundWorker1.RunWorkerAsync();
         }
 
         private void popup_MouseMove(object sender, MouseEventArgs e)
         {
-            if (!popup.IsOpen)
-                popup.IsOpen = true;
-
             var mousePosition = e.GetPosition(myWindow);
             popup.HorizontalOffset = mousePosition.X+20;
             popup.VerticalOffset = mousePosition.Y-20;
+        }
 
+        private void backgroundWorker1_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+                Thread.Sleep(1000);
+                backgroundWorker1.ReportProgress(100);
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+        {
+            popupText.Text = "";
         }
     }
 }
