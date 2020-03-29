73743a57a3474ca1b94253b45410f7326186ecb2
Rafal <jagielski.rafal.uwm@tlen.pl>
2020-03-29 14:47:33 +0200

    Delete unused directives

--- graph_first/graph_first/MainWindow.xaml.cs
+++ graph_first/graph_first/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
+﻿using System.Threading;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.ComponentModel;
 
 namespace graph_first
@@ -45,12 +34,9 @@ namespace graph_first
 
         private void copyTreeBtn(object sender, RoutedEventArgs e)
         {
-
             Clipboard.SetText(treeTextField.Text);
             popupText.Text = "Tree copied to clipboard";
             backgroundWorker1.RunWorkerAsync();
-
-
         }
         private void copySolutionBtn(object sender, RoutedEventArgs e)
         {
--- graph_first/graph_first/Tree.cs
+++ graph_first/graph_first/Tree.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace graph_first
 {
@@ -10,7 +8,7 @@ namespace graph_first
     {
         public List<Node> _nodes;
         public List<Edge> _edges;
-        public Node _root { get; set; }
+        public Node _root { get; private set; }
 
         public Tree()
         {
